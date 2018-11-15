b32c4155c9745c2884ffb443085360a7b620becf
Alec Wenzowski <alec@wenzowski.com>
2018-11-15 17:34:14 -0500

    Button uses 1Password

---
+++ scripts/common/applications-button.sh
@@ -0,0 +1,12 @@
+# All these applications are independent, so if one
+# fails to install, don't stop.
+set +e
+
+echo
+echo "Installing Button-specific applications"
+
+# Password Management
+
+brew cask install 1password
+brew cask install 1password-cli
+set -e
